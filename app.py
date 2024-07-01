178f869d555c6e22911743495a933af37f3b56f4
Pinkbutterfly <codesforte@CODESFORTEs-MacBook-Pro.local>
2024-07-01 16:08:15 -0400

    final app update

--- app.py
+++ app.py
@@ -7,29 +7,29 @@ notes = []
 
 @app.route('/')
 def index():
-    load_notes()
+    loadNotes()
     return render_template('index.html', notes=notes)
 
-@app.route('/add_note', methods = ['POST'])
-def add_note():
+@app.route('/add', methods = ['POST'])
+def addNote():
     note_text = request.form['note_text']
     note_color = request.form['note_color']
     # Add note to list of notes
     notes.append({'text': note_text, 'color': note_color})
-    save_notes()  # Save note to file
+    saveNotes()  # Save note to file
     return index()
 
-@app.route('/delete_note/<int:note_id>', methods = ['GET', 'POST'])
-def delete_note(note_id):
+@app.route('/delete/<int:note_id>', methods = ['GET', 'POST'])
+def deleteNote(note_id):
     if request.method == 'POST':
         # Delete the note with the stated id from list of notes
         del notes[note_id]
-        save_notes() # Save note to file
+        saveNotes() # Save note to file
     # Return the updated list
     return index()
 
 
-def load_notes():
+def loadNotes():
     global notes
     try:
         with open('notes.json', 'r') as file:
@@ -37,7 +37,7 @@ def load_notes():
     except FileNotFoundError:
         notes = []
 
-def save_notes():
+def saveNotes():
     with open('notes.json', 'w') as file:
         json.dump(notes, file, indent=4)
 
