ce19142afc61037356a88cdd342e3d8bb416336e
Pinkbutterfly <codesforte@CODESFORTEs-MacBook-Pro.local>
2024-07-01 13:35:15 -0400

    update on application

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ def index():
     load_notes()
     return render_template('index.html', notes=notes)
 
+@app.route('/add_note', methods = ['POST'])
+def add_note():
+    note_text = request.form['note_text']
+    note_color = request.form['note_color']
+    # Add note to list of notes
+    notes.append({'text': note_text, 'color': note_color})
+    save_notes()  # Save note to file
+    return index()
+
+@app.route('/delete_note/<int:note_id>', methods = ['GET', 'POST'])
+def delete_note(note_id):
+    if request.method == 'POST':
+        # Delete the note with the stated id from list of notes
+        del notes[note_id]
+        save_notes() # Save note to file
+    # Return the updated list
+    return index()
+
+
 def load_notes():
     global notes
     try:
@@ -17,6 +36,10 @@ def load_notes():
             notes = json.load(file)
     except FileNotFoundError:
         notes = []
+
+def save_notes():
+    with open('notes.json', 'w') as file:
+        json.dump(notes, file, indent=4)
 
 if __name__ == '__main__':
     app.run(debug=True)
